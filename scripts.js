0f992af5c960f6b700e43051e753b6329f7b7837
Prokai <prokaiindo003@gmail.com>
2022-09-12 19:38:36 +0530

    edited the search eninge

--- scripts.js
+++ scripts.js
@@ -9,7 +9,7 @@ const searchInput = document.querySelector("#searchbar > input")
 const searchButton = document.querySelector("#searchbar > button")
 
 const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/","yt":"https://youtube.com/","dc":"https://discord.com/channels/@me","spfy":"https://open.spotify.com/","yugen":"https://yugen.to","9anime":"https://9anime.id/home","zoro":"https://zoro.to/home","shiro":"https://shiro.is/home","animix":"https://animixplay.to","pahe":"https://animepahe.com","ash":"https://ashanime.pro/home","anlst":"https://anilist.co/home","mal":"https://myanimelist.net","kitsu":"https://kitsu.io","simkl":"https://simkl.com/anime/"}
-const engine = "duckduckgo"
+const engine = "startpage"
 const engineUrls = {
   deepl: "https://www.deepl.com/translator#-/-/",
   duckduckgo: "https://duckduckgo.com/?q=",
