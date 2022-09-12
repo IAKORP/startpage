3710ac68fd175d894ecd482365309597e252102b
Prokai <prokaiindo003@gmail.com>
2022-09-12 21:00:08 +0530

    changed some lookups

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@
 const searchInput = document.querySelector("#searchbar > input")
 const searchButton = document.querySelector("#searchbar > button")
 
-const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/","yt":"https://youtube.com/","dc":"https://discord.com/channels/@me","spfy":"https://open.spotify.com/","yugen":"https://yugen.to","9anime":"https://9anime.id/home","zoro":"https://zoro.to/home","shiro":"https://shiro.is/home","animix":"https://animixplay.to","pahe":"https://animepahe.com","ash":"https://ashanime.pro/home","anlst":"https://anilist.co/home","mal":"https://myanimelist.net","kitsu":"https://kitsu.io","simkl":"https://simkl.com/anime/"}
+const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/","yt":"https://youtube.com/","dc":"https://discord.com/channels/@me","spfy":"https://open.spotify.com/","yugen":"https://yugen.to","9anime":"https://9anime.id/home","zoro":"https://zoro.to/home","shiro":"https://shiro.is/home","animix":"https://animixplay.to","pahe":"https://animepahe.com","ash":"https://ashanime.pro/home","anlst":"https://anilist.co/home","mal":"https://myanimelist.net","kitsu":"https://kitsu.io","simkl":"https://simkl.com/anime/","dex":"https://mangadex.org","cmkck":"https://comick.fun","mnwa":"https://manhuaplus.com","mreder":"https://mangareader.to"}
 const engine = "searx"
 const engineUrls = {
   deepl: "https://www.deepl.com/translator#-/-/",
